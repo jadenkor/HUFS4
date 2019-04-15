f103fcda67412a49d541a9eb6079bc9a4cf99dd5
Jaden <ryu2208@naver.com>
2019-04-15 19:35:05 +0900

    로그인 처리를 위한 클래스 LoginRequest 추가 및 LoginActivity 수정

--- app/src/main/java/com/example/hufs4/LoginActivity.java
+++ app/src/main/java/com/example/hufs4/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.example.hufs4;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,6 +40,39 @@ public class LoginActivity extends AppCompatActivity {
                 String userID = idText.getText().toString();
                 String userPassword = passwordText.getText().toString();
 
+//DB 만들고 나면 주석 해제 하고 바로 아래 admin 관련 코드 주석 처리 or 삭제 하면 됨.
+//                Response.Listener<String> responseListener = new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        try{
+//                            JSONObject jsonResponse = new JSONObject(response);
+//                            boolean success = jsonResponse.getBoolean("success");
+//                            if(success) {
+//                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+//                                dialog = builder.setMessage("로그인에 성공했습니다.")
+//                                        .setPositiveButton("확인", null)
+//                                        .create();
+//                                dialog.show();
+//                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                                LoginActivity.this.startActivity(intent);
+//                                finish();
+//                            }
+//                            else {
+//                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+//                                dialog = builder.setMessage("학번 또는 비밀번호를 확인해주세요.")
+//                                        .setNegativeButton("다시 시도", null)
+//                                        .create();
+//                                dialog.show();
+//                            }
+//                        } catch(Exception e){
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                };
+//                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+//                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+//                queue.add(loginRequest);
+
                 if(userID.equals("admin") && userPassword.equals("admin")){
                     AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                     dialog = builder.setMessage("로그인에 성공했습니다.")
@@ -57,11 +90,7 @@ public class LoginActivity extends AppCompatActivity {
                             .setNegativeButton("다시 시도", null)
                             .create();
                     dialog.show();
-
                 }
-
-
-
             }
         });
 
