34e5bbeb8c36b75c844de06765117d8eb23af102
Jaden <ryu2208@naver.com>
2019-04-16 21:10:04 +0900

    요일/교시 설정을 위한 DayPeriodSettingActivity.java 생성

---
+++ app/src/main/java/com/example/hufs4/DayPeriodSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.hufs4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayPeriodSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_period_setting);
+    }
+}
